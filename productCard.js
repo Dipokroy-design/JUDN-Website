599c3a3e1c6f1e2a3df2127cd04c61199123f3ce
DipokRay <dipokray1100@gmail.com>
2025-08-06 21:23:41 +0600

    new improvement

--- productCard.js
+++ productCard.js
@@ -1,8 +1,8 @@
 const products = [
   {
     id: 1,
-    name: "Series-A Shirt",
-    price: "৳800.00",
+    name: "Zara",
+    price: "৳799.00",
     tag: "New",
     description: "Premium cotton, bold style.",
     image:
@@ -10,7 +10,7 @@ const products = [
   },
   {
     id: 2,
-    name: "Series-B Hoodie",
+    name: "BHPC",
     price: "৳1200.00",
     tag: "Best Seller",
     description: "Warm, comfy, standout.",
@@ -19,8 +19,8 @@ const products = [
   },
   {
     id: 3,
-    name: "Series-C Pants",
-    price: "৳950.00",
+    name: "H&M",
+    price: "৳949.00",
     tag: "Popular",
     description: "Modern fit, durable fabric.",
     image:
@@ -28,8 +28,8 @@ const products = [
   },
   {
     id: 4,
-    name: "Series-D Jacket",
-    price: "৳1800.00",
+    name: "H&M",
+    price: "৳799.00",
     tag: "Limited",
     description: "Statement piece, all-weather.",
     image:
@@ -37,12 +37,183 @@ const products = [
   },
   {
     id: 5,
-    name: "Series-D Jacket",
-    price: "৳1800.00",
+    name: "Zara",
+    price: "৳800.00",
+    tag: "Limited",
+    description: "Premium cotton, bold style.",
+    image:
+      "https://i.pinimg.com/736x/57/5c/23/575c2341b0db171e2524a73e504bd7fe.jpg",
+  },
+  {
+    id: 6,
+    name: "Zara",
+    price: "৳800.00",
+    tag: "New",
+    description: "Statement piece, all-weather.",
+    image:
+      "https://i.pinimg.com/736x/3b/e3/c9/3be3c9405e053752be15c00a6d96c454.jpg",
+  },
+  {
+    id: 7,
+    name: "Eton",
+    price: "৳999.00",
+    tag: "Limited",
+    description: "Premium cotton, bold style.",
+    image:
+      "https://i.pinimg.com/736x/41/b7/c3/41b7c3eec3525ba7b2191f42094ca8f1.jpg",
+  },
+  {
+    id: 8,
+    name: "BHPC",
+    price: "৳799.00",
+    tag: "New",
+    description: "Premium cotton, bold style.",
+    image:
+      "https://i.pinimg.com/736x/85/45/1c/85451c62cb3d224d99bbead6641606e1.jpg",
+  },
+  {
+    id: 9,
+    name: "BHPC",
+    price: "৳800.00",
+    tag: "Popular",
+    description: "Premium cotton, bold style.",
+    image:
+      "https://i.pinimg.com/736x/af/06/2f/af062f08da5ee8e158f7a96640e0da15.jpg",
+  },
+  {
+    id: 10,
+    name: "H&M",
+    price: "৳800.00",
+    tag: "New",
+    description: "Warm, comfy, standout.",
+    image:
+      "https://i.pinimg.com/736x/15/de/9c/15de9ce0aa2e951db2bb096d7de0bbcd.jpg",
+  },
+  {
+    id: 11,
+    name: "Zara",
+    price: "৳800.00",
+    tag: "Best Seller",
+    description: "Modern fit, durable fabric.",
+    image:
+      "https://i.pinimg.com/736x/d5/69/95/d569954e1fe44045a1ea3301ed2a8d29.jpg",
+  },
+  {
+    id: 12,
+    name: "H&M",
+    price: "৳849.00",
+    tag: "Best Seller",
+    description: "Modern fit, durable fabric.",
+    image:
+      "https://i.pinimg.com/1200x/81/6d/bc/816dbca7942cc46c9195b579f7f05d98.jpg",
+  },
+  {
+    id: 13,
+    name: "H&M",
+    price: "৳949.00",
+    tag: "Popular",
+    description: "Modern fit, durable fabric.",
+    image:
+      "https://i.pinimg.com/736x/52/f0/0e/52f00e9ff29db3a589a347acaec1f91f.jpg",
+  },
+  {
+    id: 14,
+    name: "Zara",
+    price: "৳949.00",
+    tag: "Best Seller",
+    description: "Modern fit, durable fabric.",
+    image:
+      "https://i.pinimg.com/1200x/86/ed/ee/86edee2c63015609dce0a5b6836bca03.jpg",
+  },
+  {
+    id: 15,
+    name: "Tom Ford",
+    price: "৳800.00",
+    tag: "New",
+    description: "Warm, comfy, standout.",
+    image:
+      "https://i.pinimg.com/736x/22/cc/a9/22cca9669c6c59ab7e3dc93fdec3a9e0.jpg",
+  },
+  {
+    id: 16,
+    name: "Zara",
+    price: "৳800.00",
     tag: "Limited",
     description: "Statement piece, all-weather.",
     image:
-      "https://i.pinimg.com/736x/57/5c/23/575c2341b0db171e2524a73e504bd7fe.jpg",
+      "https://i.pinimg.com/1200x/e1/6a/e5/e16ae5df4e2043f83a23abe487ebb405.jpg",
+  },
+  {
+    id: 17,
+    name: "Turnbull & Asser",
+    price: "৳999.00",
+    tag: "Limited",
+    description: "Premium cotton, bold style.",
+    image:
+      "https://i.pinimg.com/736x/72/4e/d2/724ed2b6df8deb5a50edf389ab52a04b.jpg",
+  },
+  {
+    id: 18,
+    name: "Brioni",
+    price: "৳949.00",
+    tag: "Limited",
+    description: "Statement piece, all-weather.",
+    image:
+      "https://i.pinimg.com/736x/7f/fb/47/7ffb474c694ac0e6dc7fc7b5cbc85294.jpg",
+  },
+  {
+    id: 19,
+    name: "J.Crew",
+    price: "৳800.00",
+    tag: "New",
+    description: "Premium cotton, bold style.",
+    image:
+      "https://i.pinimg.com/736x/e3/b5/f2/e3b5f207929f42a0aae717e3f6cf4792.jpg",
+  },
+  {
+    id: 20,
+    name: "J.Crew",
+    price: "৳800.00",
+    tag: "New",
+    description: "Premium cotton, bold style.",
+    image:
+      "https://i.pinimg.com/736x/70/0d/8d/700d8deed6cce77a7855bcf5a215e34e.jpg",
+  },
+  {
+    id: 21,
+    name: "J.Crew",
+    price: "৳800.00",
+    tag: "New",
+    description: "Premium cotton, bold style.",
+    image:
+      "https://i.pinimg.com/736x/bb/9b/6c/bb9b6c06eac1c4649bf88c3e5652eb95.jpg",
+  },
+  {
+    id: 22,
+    name: "Bonobos",
+    price: "৳800.00",
+    tag: "New",
+    description: "Modern fit, durable fabric.",
+    image:
+      "https://i.pinimg.com/1200x/f3/20/e1/f320e182c83ae9691445737c86e6ac80.jpg",
+  },
+  {
+    id: 23,
+    name: "Zara",
+    price: "৳800.00",
+    tag: "Popular",
+    description: "Statement piece, all-weather.",
+    image:
+      "https://i.pinimg.com/736x/e2/9b/01/e29b01f723282f1240fc56d25d9d5b7c.jpg",
+  },
+  {
+    id: 24,
+    name: "Zara",
+    price: "৳800.00",
+    tag: "Popular",
+    description: "Statement piece, all-weather.",
+    image:
+      "https://i.pinimg.com/1200x/ca/10/d4/ca10d42d33dd7dcad71e1e67286a3e28.jpg",
   },
 ];
 
